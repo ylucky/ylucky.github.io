5c322649e471e3652a676373b758b6a2882ec578
zhangdan <1023213992@qq.com>
2018-09-09 16:32:22 +0800

    update

--- web/cert/js/index.js
+++ web/cert/js/index.js
@@ -10,52 +10,13 @@ var compiledRendered = compiled(cert);
 document.getElementById('content-wrap').innerHTML = compiledRendered;
 
 //横屏
-/*var evt = "onorientationchange" in window ? "orientationchange" : "resize";
-      
-window.addEventListener(evt, function() {
-console.log(evt);
-	var width = document.documentElement.clientWidth;
-	var height =  document.documentElement.clientHeight;
-	$print =  document.getElementById('container');
-	if( width > height ){
-
-		$print.style.width=width;
-		$print.style.height=height;
-		$print.style.top=0;
-		$print.style.left=0;
-		$print.style.transform='none';
-		$print.style.transformOrigin='50% 50%';
-	}
-	else{
-		$print.style.width=height;
-		$print.style.height=width;
-		$print.style.top=(height-width)/2 ;
-		$print.style.left=0-(height-width)/2 ;
-		$print.style.transform='rotate(90deg)';
-		$print.style.transformOrigin='50% 50%';
-	}
-
-}, false);
-*/
-
-//判断手机横竖屏状态：  
-   /* window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {  
-            if (window.orientation === 180 || window.orientation === 0) {   
-               alert('竖屏状态！');  
-            }   
-            if (window.orientation === 90 || window.orientation === -90 ){   
-                alert('横屏状态！');  
-            }    
-        }, false); */
-
-
 (function () {   
     function resize() {
         var body = document.getElementsByTagName('body')[0];
         var html = document.getElementsByTagName('html')[0];
         var width = html.clientWidth;
         var height =  html.clientHeight;
-        var max = width > height ? height : width;
+        var max = width > height ? (4/3)*height : width;
         document.getElementById('container').style.width = max + "px";
 
     }
